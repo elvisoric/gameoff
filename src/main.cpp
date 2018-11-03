1fce6efc8a7d39995702804813c0f8c422b9e649
Elvis Oric <elvis.oric90@gmail.com>
2018-11-03 10:23:03 +0100

    Add shader program and basic shader implementation of shader program. Add shader implementations for basic shader, basic.vs and basic.fs.

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,19 @@
 #include <glad/glad.h>
 
+#include <impl/basic_shader.h>
 #include <window.h>
 #include <helper.hpp>
 
 int main() {
   auto window = jam::createWindow(1024.0f, 768.0f);
 
+  jam::shader::BasicShader shader;
+
   while (!window.shouldClose()) {
     // TODO: resolve clearing screen in different way
     glClear(GL_COLOR_BUFFER_BIT);
+    shader.start();
+    shader.stop();
 
     window.pollEvents();
     window.swapBuffers();
